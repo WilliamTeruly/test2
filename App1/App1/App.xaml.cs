7bd1940a3cd21c41ba8f28844bdfd8aa7793e5a4
to Sai <william_teruly@msn.com>
2018-01-23 20:58:03 +0900

    test

---
+++ App1/App1/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace App1
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new App1.MainPage();
+		}
+
+		protected override void OnStart ()
+        {
+            AppCenter.Start("ios=ed188bb0-0319-41e7-88a5-8e4b93450f38;" + "uwp={ed188bb0-0319-41e7-88a5-8e4b93450f38};" +
+                               "android={ed188bb0-0319-41e7-88a5-8e4b93450f38}",
+                               typeof(Analytics), typeof(Crashes));
+        }
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
